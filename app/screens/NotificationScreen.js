288a4c792f0e01a9fafec22b9b91dd2ffe7e1926
abiaugus <appus525@gmail.com>
2021-05-20 12:35:48 +0530

    Notifications

--- app/screens/NotificationScreen.js
+++ app/screens/NotificationScreen.js
@@ -1,19 +1,366 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useState} from "react";
+import { Animated, TouchableHighlight, TouchableOpacity, StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar} from "react-native";
+import { SwipeListView } from 'react-native-swipe-list-view';
 import theme from "../config/theme";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+const Notifications = [
+  {
+      id: 1,
+      title: 'Someone likes your post!',
+      details: 'Madara Uchiha liked your post.'
+  },
+  {
+      id: 2,
+      title: 'You got 14 upvotes!',
+      details: 'Your latest post has 14 upvotes now.'
+  },
+  {
+      id: 3,
+      title: 'Goal!',
+      details: 'A project you backed has met the required goal amount!'
+  },
+  {
+      id: 4,
+      title: 'SAASUKE',
+      details: 'bla bla bla bla bla.'
+  },
+  {
+      id: 5,
+      title: 'NARUTO',
+      details: 'bla bla bla bla bla.'
+  },
+  {
+      id: 6,
+      title: 'SASUKEE',
+      details: 'bla bla bla bla bla.'
+  },
+  {
+      id: 7,
+      title: 'NARUTOO',
+      details: 'bla bla bla bla bla.'
+  },
+  {
+      id: 8,
+      title: 'SASUKEEE',
+      details: 'bla bla bla bla bla.'
+  },
+  {
+      id: 9,
+      title: 'NAARUTOOO',
+      details:'bla bla bla bla bla.'
+  },
+  {
+      id: 10,
+      title: 'SAAASUUKEEE',
+      details: 'bla bla bla bla bla.'
+  },
+  {
+    id: 11,
+    title: 'NAARUTOOOO',
+    details: 'bla bla bla bla bla.'
+  }, 
+  {
+    id: 12,
+    title: 'SAASUUU',
+    details: 'bla bla bla bla bla.'
+  },
+  {
+    id: 13,
+    title: 'NARUUU',
+    details: 'bla bla bla bla bla.'
+  },
+];
 
-export default function NotificationScreen() {
+
+const NotificationScreen = ({navigation}) => {
+  const [listData, setListData] = useState(
+    Notifications.map((NotificationItem, index) => ({
+      key: `${index}`,
+      title: NotificationItem.title,
+      details: NotificationItem.details,
+    })),
+  );
+
+  const closeRow = (rowMap, rowKey) => {
+    if (rowMap[rowKey]) {
+      rowMap[rowKey].closeRow();
+    }
+  };
+
+  const deleteRow = (rowMap, rowKey) => {
+    closeRow(rowMap, rowKey);
+    const newData = [...listData];
+    const prevIndex = listData.findIndex(item => item.key === rowKey);
+    newData.splice(prevIndex, 1);
+    setListData(newData);
+  };
+
+  const onRowDidOpen = rowKey => {
+    console.log('This row opened', rowKey);
+  };
+
+  const onLeftActionStatusChange = rowKey => {
+    console.log('onLeftActionStatusChange', rowKey);
+  };
+
+  const onRightActionStatusChange = rowKey => {
+    console.log('onRightActionStatusChange', rowKey);
+  };
+
+  const onRightAction = rowKey => {
+    console.log('onRightAction', rowKey);
+  };
+
+  const onLeftAction = rowKey => {
+    console.log('onLeftAction', rowKey);
+  };
+
+  const VisibleItem = props => {
+    const {
+      data,
+      rowHeightAnimatedValue,
+      removeRow,
+      leftActionState,
+      rightActionState,
+    } = props;
+
+    if (rightActionState) {
+      Animated.timing(rowHeightAnimatedValue, {
+        toValue: 0,
+        duration: 200,
+        useNativeDriver: false,
+      }).start(() => {
+        removeRow();
+      });
+    }
+
+    return (
+      <SafeAreaView>
+      <Animated.View
+        style={[styles.rowFront, {height: rowHeightAnimatedValue}]}>
+        <TouchableHighlight
+          style={styles.rowFrontVisible}
+          onPress={() => console.log('Element touched')}
+          underlayColor={'#aaa'}>
+          <View>
+            <Text style={styles.title} numberOfLines={1}>
+              {data.item.title}
+            </Text>
+            <Text style={styles.details} numberOfLines={1}>
+              {data.item.details}
+            </Text>
+          </View>
+        </TouchableHighlight>
+      </Animated.View>
+      </SafeAreaView>
+    );
+  };
+
+  const renderItem = (data, rowMap) => {
+    const rowHeightAnimatedValue = new Animated.Value(60);
+
+    return (
+      <VisibleItem
+        data={data}
+        rowHeightAnimatedValue={rowHeightAnimatedValue}
+        removeRow={() => deleteRow(rowMap, data.item.key)}
+      />
+    );
+  };
+
+  const HiddenItemWithActions = props => {
+    const {
+      swipeAnimatedValue,
+      leftActionActivated,
+      rightActionActivated,
+      rowActionAnimatedValue,
+      rowHeightAnimatedValue,
+      onClose,
+      onDelete,
+    } = props;
+
+    if (rightActionActivated) {
+      Animated.spring(rowActionAnimatedValue, {
+        toValue: 500,
+        useNativeDriver: false
+      }).start();
+    } else {
+      Animated.spring(rowActionAnimatedValue, {
+        toValue: 75,
+        useNativeDriver: false
+      }).start();
+    }
+
+    return (
+      <Animated.View style={[styles.rowBack, {height: rowHeightAnimatedValue}]}>
+        <Text>Left</Text>
+        {!leftActionActivated && (
+          <TouchableOpacity
+            style={[styles.backRightBtn, styles.backRightBtnLeft]}
+            onPress={onClose}>
+            <MaterialCommunityIcons
+              name="close-circle-outline"
+              size={25}
+              style={styles.trash}
+              color="#fff"
+            />
+          </TouchableOpacity>
+        )}
+        {!leftActionActivated && (
+          <Animated.View
+            style={[
+              styles.backRightBtn,
+              styles.backRightBtnRight,
+              {
+                flex: 1,
+                width: rowActionAnimatedValue,
+              },
+            ]}>
+            <TouchableOpacity
+              style={[styles.backRightBtn, styles.backRightBtnRight]}
+              onPress={onDelete}>
+              <Animated.View
+                style={[
+                  styles.trash,
+                  {
+                    transform: [
+                      {
+                        scale: swipeAnimatedValue.interpolate({
+                          inputRange: [-90, -45],
+                          outputRange: [1, 0],
+                          extrapolate: 'clamp',
+                        }),
+                      },
+                    ],
+                  },
+                ]}>
+                <MaterialCommunityIcons
+                  name="trash-can-outline"
+                  size={25}
+                  color="#fff"
+                />
+              </Animated.View>
+            </TouchableOpacity>
+          </Animated.View>
+        )}
+      </Animated.View>
+    );
+  };
+
+  const renderHiddenItem = (data, rowMap) => {
+    const rowActionAnimatedValue = new Animated.Value(75);
+    const rowHeightAnimatedValue = new Animated.Value(60);
+
+    return (
+      <HiddenItemWithActions
+        data={data}
+        rowMap={rowMap}
+        rowActionAnimatedValue={rowActionAnimatedValue}
+        rowHeightAnimatedValue={rowHeightAnimatedValue}
+        onClose={() => closeRow(rowMap, data.item.key)}
+        onDelete={() => deleteRow(rowMap, data.item.key)}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={{ color: theme.colors.white }}>Notification Screen</Text>
+      <StatusBar barStyle="dark-content"/>
+      {/* <StatusBar backgroundColor="#FF6347" barStyle="light-content"/> */}
+      <SwipeListView style={{marginTop:50}}
+        data={listData}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        leftOpenValue={75}
+        rightOpenValue={-150}
+        disableRightSwipe
+        onRowDidOpen={onRowDidOpen}
+        leftActivationValue={100}
+        rightActivationValue={-200}
+        leftActionValue={0}
+        rightActionValue={-500}
+        onLeftAction={onLeftAction}
+        onRightAction={onRightAction}
+        onLeftActionStatusChange={onLeftActionStatusChange}
+        onRightActionStatusChange={onRightActionStatusChange}
+      />
     </View>
   );
-}
+};
+
+
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#464B58',
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  },
+  backTextWhite: {
+    color: '#FFF',
+  },
+  rowFront: {
+    backgroundColor: '#7e88a3',
+    borderRadius: 5,
+    height: 60,
+    margin: 5,
+    marginBottom: 15,
+    shadowColor: '#999',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  rowFrontVisible: {
+    backgroundColor: '#7e88a3',
+    borderRadius: 5,
+    height: 60,
+    padding: 10,
+    marginBottom: 15,
+  },
+  rowBack: {
+    alignItems: 'center',
+    backgroundColor: '#DDD',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+    margin: 5,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
+  backRightBtn: {
+    alignItems: 'flex-end',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 75,
+    paddingRight: 17,
+  },
+  backRightBtnLeft: {
+    backgroundColor: '#1f65ff',
+    right: 75,
+  },
+  backRightBtnRight: {
+    backgroundColor: 'red',
+    right: 0,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  trash: {
+    height: 25,
+    width: 25,
+    marginRight: 7,
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#FFF',
+  },
+  details: {
+    fontSize: 12,
+    color: '#FFF',
   },
 });
+export default NotificationScreen;
